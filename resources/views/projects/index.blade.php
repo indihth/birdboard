5fdbcc7e9fbba4e5fe834fe10acad5a062478924
indihth <86363851+indihth@users.noreply.github.com>
2023-05-22 15:44:19 +0100

    Basic index and element styling Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,8 +4,23 @@
     <div class="flex items-center mb-3">
         <a href="/projects/create">New project</a>
     </div>
-    <ol>
 
+    <div class="flex">
+        @forelse ($projects as $project) 
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height:200px;">
+            <h3 class="font-normal text-xl m-0 py-4">{{ $project->title }}</h3>
+            {{-- <h3>{{ Illuminate\Support\Str::limit($project->title, 300) }}</h3> --}}
+            
+            <div class="text-grey">{{ Str::limit($project->description, 100) }}</div>
+        </div>
+
+        @empty
+
+        <div class="">No projects yet.</div>
+
+        @endforelse
+    </div>
+    {{-- <ul>
         @forelse ($projects as $project)
             <li>
                 <a href="{{ $project->path() }}">{{ $project->title }}</a>
@@ -14,5 +29,5 @@
         @empty
             <li>No projects yet.</li>
         @endforelse
-    </ol>
+    </ul> --}}
 @endsection
